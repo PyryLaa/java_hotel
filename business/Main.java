47264e2ffa39393689cc414769cc3f8da0ff31ac
pyryl <pyry.laaksonen@hotmail.com>
2024-02-03 12:33:37 +0200

    User login done, to do: implement files

--- business/Main.java
+++ business/Main.java
@@ -1,13 +1,33 @@
 package business;
 import java.util.Scanner;
 import java.util.ArrayList;
-import java.io.Console;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 
 public class Main {
     static Scanner scan = new Scanner(System.in);
+    //Save employee's and customer's data to these files
+    static File emp_file = new File("emps.txt");
+    static File cust_file = new File("cust.txt");
     public static void main(String[] args){
         Integer role = 0;
+        try{
+            if(emp_file.createNewFile()){
+                System.out.println("Emp file created!");
+            }else{
+                System.out.println("Emp file exists");
+            }
+            if(cust_file.createNewFile()){
+                System.out.println("Customer file created");
+            }else{
+                System.out.println("Customer file exists");
+            }
+        }catch(IOException e){
+            System.out.println("Error occurred!");
+            e.printStackTrace();
+        }
         
         ArrayList<Customer> customers = new ArrayList<Customer>();
         ArrayList<Employee> employees = new ArrayList<Employee>();
@@ -85,10 +105,32 @@ public class Main {
         
     }
     static void ret_customer(ArrayList<Customer> cust){
-        String input;
-        System.out.println("Give username: ");
-        input = scan.nextLine();
-        System.out.println("Welcome (insert customer name here) to Business!");
+        String un, pass;
+        boolean valid = false;
+        int index = -1; //-1 to check if username is found or not
+        do{
+            System.out.println("Username: ");
+            un = scan.nextLine();
+            System.out.println("Password: ");
+            pass = scan.nextLine();
+            for(int i = 0; i < cust.size(); i++){ //Check if the username is found
+                if(cust.get(i).getUname().equals(un)){
+                    index = i;
+                    break;
+                }
+            }
+            if(index == -1){
+                System.out.println("Username not found!");
+            }else{
+                if(!(cust.get(index).getPassword().equals(pass))){
+                    System.out.println("Invalid password!");
+                }else{
+                    valid = true;
+                }
+            }
+        }while(valid == false);
+        System.out.println("Welcome " + cust.get(index).getFname() + " " + cust.get(index).getLname() + " to Business!");
+
     }
     static void emp(ArrayList<Employee> emp){
         System.out.println("Hello, (employee name here)!");
